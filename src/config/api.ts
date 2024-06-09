a45e23269f95b0ac4e07687efa0cb7654a3205cc
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-06-08 21:39:03 -0300

    signin conectado con back

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,11 @@
-export const API_URL_BASE = import.meta.env.VITE_URL_BASE;
+export const API_URL_BASE = "http://localhost:3001/api/v1";
+
+
+//-------------AUTH-----------------------------------------
+export const API_URL_AUTH = API_URL_BASE + "/auth"
+
+export const API_URL_REGISTER = API_URL_AUTH + "/register"
+export const API_URL_SIGN_IN = API_URL_AUTH + "/signin"
+export const API_URL_PROFILE = API_URL_AUTH +"/profile"
+export const API_URL_SIGN_OUT = API_URL_AUTH + "/signout" // Falta costruir en el backend
+ 
