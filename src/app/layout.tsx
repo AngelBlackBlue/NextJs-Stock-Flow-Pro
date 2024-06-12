48e92593d1d8ac2d5a84460f9d9ff308d38ee8e4
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-06-12 13:22:06 -0300

    NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import QueryProvider from "@/components/query-provider";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,15 +20,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
-      <ThemeProvider
+        <QueryProvider>
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
-          >
-            {children}
+            >
+              <main className="max-w-6xl min-h-screen mx-auto py-10 space-y-10">
+                <Navbar/>
+                {children}
+              </main>
           </ThemeProvider>
-        </body>
+        </QueryProvider>
+      </body>
     </html>
   );
 }
